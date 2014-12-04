2f1ea45f9ec2e24b7f39669b880357ec87589293
ctrembath <claretrembath@ymail.com>
2014-12-04 14:49:46 +0000

    finished basic functionality

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,8 +1,10 @@
-GithubUserSearch.controller('GitUserSearchController', function($scope, $http){
-  var onUserComplete = function(response){
-    $scope.user = response.data;
+GithubUserSearch.controller('GitUserSearchController', function($scope, $resource){
+  
+  var searchResource = $resource('https://api.github.com/search/users?access_token=1e9bc970163f5ec8631a526231cb16088f8b1ea6');
+
+  $scope.doSearch = function (){
+    $scope.searchResult = searchResource.get({
+      q: $scope.searchTerm
+    });
   };
-
-  $http.get('https://api.github.com/users/giorgia-amici').then(onUserComplete);
 });
-
